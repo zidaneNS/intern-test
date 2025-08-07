c38d45ab3b41014a36b5ab4c1ad1dded79bd1f58
zidane.nur004 <zidane.nur004@gmail.com>
2025-08-07 10:10:51 +0000

    case-3 home add section

--- case_3/src/app/app.ts
+++ case_3/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Nav } from './nav/nav';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, Nav],
+  imports: [RouterModule, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
